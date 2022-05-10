d35850a1a5b6e3cdb93bc5f142552350616ff433
behrooz <behrooz.zendedel1992@gmail.com>
2022-05-10 14:12:45 +0430

    added portfolio component and swiper and swiper slider

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,7 +1,39 @@
-import React from "react";
 import "./Portfolio.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import portfolio1 from "../../assets/3.png";
+import portfolio2 from "../../assets/2.png";
+import portfolio3 from "../../assets/1.png";
+import portfolio4 from "../../assets/4.png";
 const Portfolio = () => {
-  return 
+  return (
+    //   hedaing
+    <div className="portfolio">
+      <span>Recent Project</span>
+      <span>Portfolio</span>
+      {/* slider */}
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={3}
+        grabCursor={true}
+        className="portfolio-slider"
+      >
+        <SwiperSlide>
+          <img src={portfolio1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={portfolio2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={portfolio3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={portfolio4} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default Portfolio;
